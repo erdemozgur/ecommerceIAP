c47687f6df7f2d6fe94401bcbdd69c294618f1e3
erdem ozgur <erdmzgr@gmail.com>
2020-03-30 13:25:51 +0300

    Create a PopUp Container and Animate It Off The Screen When Dismissed.
    Create a PopUp Container and Animate It Off The Screen When Dismissed.

--- ecommerceIAP/ecommerceIAP/Custom Items/MCPopup.swift
+++ ecommerceIAP/ecommerceIAP/Custom Items/MCPopup.swift
@@ -11,18 +11,36 @@ import UIKit
 
 class MCPopup: UIView {
     
+    //Properties
+    
+    //IUVisualEffect
+    fileprivate var effect: UIVisualEffect = UIBlurEffect(style: .prominent)
+    fileprivate lazy var blurView: UIVisualEffectView = UIVisualEffectView(effect: effect)
+    
+    fileprivate let container: UIView = {
+        
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .white
+        view.layer.cornerRadius = 24
+        
+        return view
+        
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         setupViews()
         animateIn()
+        animateOut()
         addGestureRecognizer(UIGestureRecognizer(target: self, action: #selector(animateOut)))
 
         
     }
-    //IUVisualEffect
-    fileprivate var effect: UIVisualEffect = UIBlurEffect(style: .prominent)
-    fileprivate lazy var blurView: UIVisualEffectView = UIVisualEffectView(effect: effect)
+
+    
+    
     
     
     fileprivate func setupViews() {
@@ -30,16 +48,37 @@ class MCPopup: UIView {
         let screenFrame = UIScreen.main.bounds
         self.frame = screenFrame
         blurView.frame = screenFrame
+        
+        self.backgroundColor = UIColor.gray.withAlphaComponent(0.5)
         self.addSubview(blurView)
+        blurView.contentView.addSubview(container)
+        
+        container.heightAnchor.constraint(equalTo: blurView.heightAnchor, multiplier: 0.45).isActive = true
+        container.widthAnchor.constraint(equalTo: blurView.widthAnchor, multiplier: 0.7).isActive = true
+        container.centerYAnchor.constraint(equalTo: blurView.centerYAnchor).isActive = true
+        container.centerXAnchor.constraint(equalTo: blurView.centerXAnchor).isActive = true
+
+        
         
     }
     
     fileprivate func animateIn() {
         
+        
     }
     
     @objc fileprivate func animateOut(){
         
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 2, options: .curveEaseOut, animations: {
+            
+            self.container.transform = CGAffineTransform(translationX: 0, y: -self.frame.height)
+            self.alpha = 0
+            
+        }) { (complete) in
+            if complete {
+                self.removeFromSuperview()
+            }
+        }
     }
     
 

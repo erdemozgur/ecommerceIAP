bea368ebdec840befd688c02e0149701a034d5e6
erdem ozgur <erdmzgr@gmail.com>
2020-03-30 14:04:33 +0300

    Add Custom Views to MCPopup with StackViews, NSLayoutCostraints, and Live Code.
    Add Custom Views to MCPopup with StackViews, NSLayoutCostraints, and Live Code.

--- ecommerceIAP/ecommerceIAP/Custom Items/MCPopup.swift
+++ ecommerceIAP/ecommerceIAP/Custom Items/MCPopup.swift
@@ -28,6 +28,51 @@ class MCPopup: UIView {
         
     }()
     
+    fileprivate let titleLabel: UILabel = {
+        
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        label.text = "buy another credit"
+        label.textAlignment = .center
+        label.numberOfLines = 2
+        
+        return label
+    }()
+    
+    fileprivate let oneCreditLabel: UILabel = {
+        
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        label.text = "one credit"
+        label.textAlignment = .center
+        
+        return label
+    }()
+    
+    fileprivate let priceLabel: UILabel = {
+        
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        label.text = "$4.99"
+        label.textAlignment = .center
+        
+        return label
+    }()
+    
+    let buyButton = MCButton(text: "Buy")
+    
+    let oneImageView: UIImageView = {
+        
+        let oneImage = UIImage(named: "creditsymbol")
+        let iv = UIImageView(image: oneImage)
+        iv.contentMode = .scaleAspectFit
+        return iv
+        
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
@@ -36,13 +81,8 @@ class MCPopup: UIView {
         
         addGestureRecognizer(UIGestureRecognizer(target: self, action: #selector(animateOut)))
 
-        
     }
 
-    
-    
-    
-    
     fileprivate func setupViews() {
         
         let screenFrame = UIScreen.main.bounds
@@ -58,8 +98,29 @@ class MCPopup: UIView {
         container.centerYAnchor.constraint(equalTo: blurView.centerYAnchor).isActive = true
         container.centerXAnchor.constraint(equalTo: blurView.centerXAnchor).isActive = true
 
+        let arrayOfViews: [UIView] = [titleLabel,
+                            oneImageView,
+                            oneCreditLabel,
+                            priceLabel,
+                            buyButton]
+        let stack = UIStackView(arrangedSubviews: arrayOfViews)
+        stack.axis = .vertical
+        stack.distribution = .equalCentering
+        stack.spacing = 10
         
         
+        container.addSubview(stack)
+        
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.centerXAnchor.constraint(equalTo: container.centerXAnchor).isActive = true
+        stack.widthAnchor.constraint(equalToConstant: 108).isActive = true
+        stack.topAnchor.constraint(equalTo: container.topAnchor, constant: 25).isActive = true
+        stack.bottomAnchor.constraint(equalTo: container.bottomAnchor, constant: -25).isActive = true
+        
+        oneImageView.heightAnchor.constraint(equalToConstant: 80).isActive = true
+        titleLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
+
+
     }
     
     fileprivate func animateIn() {
@@ -94,7 +155,5 @@ class MCPopup: UIView {
     required init?(coder: NSCoder) {
         fatalError("init coder has not been implemented")
     }
-    
-    
-    
+
 }
